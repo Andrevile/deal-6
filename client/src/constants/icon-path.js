f36e58408da0ced4807c715c5468c57337869bab
jihoMoon <sun_days@naver.com>
2021-07-15 18:13:03 +0900

    [#32] set: :file_folder: 이미지 파일 및 path 추가

---
+++ client/src/constants/icon-path.js
@@ -0,0 +1,19 @@
+  
+export const ADD_ICON = '/client/public/images/icons/add.svg';
+export const CATEGORY_ICON = '/client/public/images/icons/category.svg';
+export const CHECK_ICON = '/client/public/images/icons/check.svg';
+export const CLOSE_ICON = '/client/public/images/icons/close.svg';
+export const DOWN_ICON = '/client/public/images/icons/down.svg';
+export const DROPDOWN_ICON = '/client/public/images/icons/dropdown.svg';
+export const HEART_ICON = '/client/public/images/icons/heart.svg';
+export const FILLED_HEART_ICON = '/client/public/images/icons/filledHeart.svg';
+export const LEFT_ICON = '/client/public/images/icons/left.svg';
+export const LOCATION_ICON = '/client/public/images/icons/location.svg';
+export const LOGOUT_ICON = '/client/public/images/icons/logout.svg';
+export const MENU_ICON = '/client/public/images/icons/menu.svg';
+export const MESSAGE_ICON = '/client/public/images/icons/message.svg';
+export const PHOTO_ICON = '/client/public/images/icons/photo.svg';
+export const RIGHT_ICON = '/client/public/images/icons/right.svg';
+export const SEND_ICON = '/client/public/images/icons/send.svg';
+export const USER_ICON = '/client/public/images/icons/user.svg';
+export const DELETE_PHOTO_ICON = '/client/public/images/icons/deletePhoto.svg';
