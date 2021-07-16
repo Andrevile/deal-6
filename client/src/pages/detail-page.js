ea6e29a2a43edec3ae30b8036e8ab65e2bd011cb
0woodev <tc_yeongu.n@woowafriends.com>
2021-07-16 13:20:25 +0900

    [#8, #9, #10] wip: :gear: 상품상세페이지 구현중 데모데이를 위한 중간 커밋
    파일명 변경 navbar -> navigator
    constants 의 icon-path 수정
    webpack.config.js devServer 속성 추가

--- client/src/pages/detail-page.js
+++ client/src/pages/detail-page.js
@@ -1,9 +1,27 @@
 import './detail-page.css';
-import Navbar from '../components/detail-page/navbar/navbar.js';
+import Navigator from '../components/detail-page/navigator/navigator.js';
+import Section from '../components/detail-page/section/section';
+// import { createDOMWithSelector } from '../util/createDOMWithSelector';
+// import Footer from '../components/detail-page/footer/';
 
-export default class Detailpage {
+export default class DetailPage {
+	state = {
+		// TEST CASE
+		seller: '0woodev',
+		price: 35000,
+		title: '신발 팝니다!',
+		description:
+			'많이 안신었던 신발 팝니다! 사이즈는 265이구요. 직거래 선호합니다!',
+		status: '판매 중',
+		town: '문래동',
+		category: '의류/잡화',
+		imgs: ['/imgs/shoes-1.jpg', '/imgs/shoes-1.jpg', '/imgs/shoes-1.jpg'],
+	};
+
 	constructor($app) {
-		this.navbar = new Navbar({ $app });
+		this.navigator = new Navigator({ $parent: $app, state: this.state });
+		this.section = new Section({ $parent: $app, state: this.state });
+		// this.footer = new Footer({ $app }, this.state);
 	}
 
 	setState() {}
