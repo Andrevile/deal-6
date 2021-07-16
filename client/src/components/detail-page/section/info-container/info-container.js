84a32c4bdb04e396fc034fba1e821436a58f339d
0woodev <tc_yeongu.n@woowafriends.com>
2021-07-16 13:14:50 +0900

    [#8, #9, #10] wip: :gear: 상품상세페이지 구현중 데모데이를 위한 중간 커밋
    svg 파일을 못찾아서webpack 설정 추가
    상세페이지에 필요한 하위컴포넌트 제작중
    util 함수 한개 추가 : createImgDOMWithSelector
    util 함수명 변경(소문자 -> 대문자) : with -> With

---
+++ client/src/components/detail-page/section/info-container/info-container.js
@@ -0,0 +1,29 @@
+import {
+	createDOMWithSelector,
+	createImgDOMWithSelector,
+} from '../../../../util/createDOMWithSelector';
+import './info-container.css';
+
+export default class InfoContainer {
+	constructor({ $parent, state }) {
+		this.state = state;
+		this.$infoContainer = createDOMWithSelector('div', '.info-container');
+		$parent.appendChild(this.$infoContainer);
+
+		this.render();
+	}
+
+	render() {
+		const content = `
+			<div class='info status'>
+				<button>${this.state.status} v </button>
+			</div>
+			<div class='info'>${this.state.title}</div>
+			<div class='info'>${this.state.category}</div>
+			<div class='info'>${this.state.description}</div>
+			<div class='info'>${this.state.seller} </div>
+		`;
+
+		this.$infoContainer.innerHTML += content;
+	}
+}
