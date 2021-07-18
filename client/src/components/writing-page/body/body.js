100aa955da7e4414668f250c126fbed814ffeabb
jihoMoon <sun_days@naver.com>
2021-07-18 18:25:44 +0900

    [#11] feat: :sparkles: body 기능 및 UI 추가
    - 모든 값 체킹 함수
    - 버튼 렌더하는 함수
    - TextArea 이벤트 추가
    - 샘플 이미지 태그 사용중

--- client/src/components/writing-page/body/body.js
+++ client/src/components/writing-page/body/body.js
@@ -1,45 +1,101 @@
 import './body.css';
 import { createDOMWithSelector } from '../../../util/createDOMWithSelector';
-
+import { CATEGORY_LIST } from '../../../constants/category-list';
 export default class Body {
-	state = '';
+	state = {
+		location: '인창동',
+		imgPath: [],
+		title: '',
+		description: '',
+		price: '',
+		category: '',
+		haveAllValue: false,
+	};
 	// state : 동네명 (string)
-	constructor({ $parent, initialState }) {
+	constructor({ $parent, initialState, refreshState }) {
 		this.state = initialState;
-		this.$target = createDOMWithSelector('form', '.post');
+		this.refreshState = refreshState;
+		this.$target = createDOMWithSelector('div', '.post');
 		$parent.appendChild(this.$target);
 
 		this.$target.innerHTML = `
-            <div class='post__imgContainer'>
-                <img src='/icons/image.svg' alt='image'>
-                <div>
-                    <span>3/10</span>
+            <div class='post__container'>
+                <div class='post__imgContainer'>
+                    <img src='/icons/image.svg' alt='image'>
+                    <div>
+                        <span>3/10</span>
+                    </div> 
+                </div>
+                <div class='post__imgOuter'>
+                    <img class='post__imgContainer' src='/imgs/photo.jpeg' alt='image'>
+                    <button class='post__removeBtn'><span>X</span></button>
+                </div>
+                <div class='post__imgOuter'>
+                    <img class='post__imgContainer' src='/imgs/photo.jpeg' alt='image'>
+                    <button class='post__removeBtn'><span>X</span></button>
+                </div>
+                <div class='post__imgOuter'>
+                    <img class='post__imgContainer' src='/imgs/photo.jpeg' alt='image'>
+                    <button class='post__removeBtn'><span>X</span></button>
+                </div>
+                <div class='post__imgOuter'>
+                    <img class='post__imgContainer' src='/imgs/photo.jpeg' alt='image'>
+                    <button class='post__removeBtn'><span>X</span></button>
+                </div>
+                <div class='post__imgOuter'>
+                    <img class='post__imgContainer' src='/imgs/photo.jpeg' alt='image'>
+                    <button class='post__removeBtn'><span>X</span></button>
+                </div>
+            </div>
+           
+            <hr>
+            <input class='post__title' type='text' placeholder="글 제목">
+            <div class='category'>
+                <span>(필수)카테고리를 선택해주세요.</span>
+                <div class='category__button'>
+                    ${this.renderButton()}
                 </div>
             </div>
             <hr>
-            <input class='post__title' type='text' placeholder="글 제목">
-            <span> (필수)카테고리를 선택해주세요.</span>
-            <hr>
             <input class='post__price' type='text' placeholder="₩ 가격(선택사항)">
             <hr>
-            <input class='post__content' type='text' placeholder="게시글 내용을 작성해주세요.">
+            <textarea class='post__content' placeholder="게시글 내용을 작성해주세요."></textarea>
         `;
+
 		this.$title = document.querySelector('.post__title');
 		this.$price = document.querySelector('.post__price');
 		this.$content = document.querySelector('.post__content');
 
 		this.$title.addEventListener('keyup', (e) => {
 			// 글자수 제한 (추후 경고 CSS 추가해도 될듯)
-			if (e.target.value.length > 30)
+			if (e.target.value.length > 30) {
 				e.target.value = e.target.value.slice(0, 30);
+			}
+			this.state.title = e.target.value;
+			this.checkHavingAllValue();
+			this.refreshState(this.state);
 		});
 		this.$price.addEventListener('keyup', (e) => {
 			// number만 받는다.
 			e.target.value = e.target.value.replace(/[^0-9]/g, '');
+			this.state.price = e.target.value;
 		});
 		this.$price.addEventListener('focusout', (e) => {
 			this.rearrangePrice(e);
 		});
+		this.$content.addEventListener('keyup', (e) => {
+			// textarea 높이 가변조절
+			this.$content.style.height = this.$content.scrollHeight + 'px';
+			this.state.description = e.target.value;
+			this.checkHavingAllValue();
+			this.refreshState(this.state);
+		});
+	}
+
+	renderButton() {
+		return CATEGORY_LIST.map((category) => {
+			return `<button>${category}</button>`;
+		}).join('');
 	}
 
 	rearrangePrice(e) {
@@ -60,5 +116,18 @@ export default class Body {
 
 		let result = '₩ ' + array.join('');
 		e.target.value = result;
+	}
+
+	checkHavingAllValue() {
+		if (
+			this.state.title.length > 0 &&
+			this.state.description.length > 0 &&
+			this.state.imgPath.length > 0 &&
+			this.state.category.length > 0
+		) {
+			this.state.haveAllValue = true;
+			return;
+		}
+		this.state.haveAllValue = false;
 	}
 }
