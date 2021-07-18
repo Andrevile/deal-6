fe98eb45a725de38cb3b9a3ae4e8e2923b70cd82
jihoMoon <sun_days@naver.com>
2021-07-18 22:06:13 +0900

    [#11] feat: :sparkles: 네비게이터 done 버튼 기능 구현 완료

--- client/src/pages/writing-page.js
+++ client/src/pages/writing-page.js
@@ -7,25 +7,35 @@ const mode = '글쓰기';
 export default class WritingPage {
 	state = {
 		location: '인창동',
-		imgPath: [],
+		imgPath: [
+			'/imgs/photo.jpeg',
+			'/imgs/photo.jpeg',
+			'/imgs/photo.jpeg',
+			'/imgs/photo.jpeg',
+			'/imgs/photo.jpeg',
+		],
 		title: '',
 		description: '',
 		price: '',
 		category: '',
 		haveAllValue: false,
 	};
-
+	// haveAllValue : 모든 값이 있어야 Navbar doneIcon 활성화 가능!
 	constructor($parent) {
 		this.navigationBar = new NavigationBar({
 			$parent,
 			initialState: mode,
+			onClick: () => {
+				// state에 username 추가하고 haveAllValue를 빼면 될듯!
+				// 게시물 post 요청 (this.state)
+			},
 		});
 		this.body = new Body({
 			$parent,
 			initialState: this.state,
 			refreshState: (nextState) => {
-				this.state = nextState;
-			},
+				this.setState(nextState);
+			}, // 부모의 State 업데이트
 		});
 		this.footer = new Footer({
 			$parent,
@@ -33,11 +43,9 @@ export default class WritingPage {
 		});
 	}
 
-	setState() {
-		//리렌더링파트
-	}
-
-	render() {
-		//렌더링파트
+	setState(nextState) {
+		this.state = nextState;
+		this.navigationBar.setState(this.state.haveAllValue);
+		this.body.setState(nextState);
 	}
 }
