52a1812176e6b77aad34f99eafe8d0cc37842c1b
jihoMoon <sun_days@naver.com>
2021-07-20 22:14:23 +0900

    [#15] feat: :sparkles: 로그인 로그아웃 내 계정 기능구현
    -userpage

--- client/src/pages/user-page.js
+++ client/src/pages/user-page.js
@@ -2,18 +2,15 @@ import AppBar from '../components/base/navigation-bar/navigation-bar';
 import Section from '../components/user-page/section/section';
 import { createDOMWithSelector } from '../util/createDOMWithSelector';
 import './user-page.css';
-
+import { navigateTo } from '../router';
 export default class UserPage {
-	isUserLogin() {
-		/* 유저가 로그인 되어 있다면, true 반환 아니면 false */
-		return false;
-	}
+	state = '';
 
+	// 오직 id만 받아오면 된다!
 	constructor($parent) {
 		this.$parent = createDOMWithSelector('div', '.userWrapper');
 		$parent.appendChild(this.$parent);
 
-		this.state = '남영우';
 		this.$appBar = new AppBar({
 			$parent: this.$parent,
 			initialState: this.isUserLogin() ? '내 계정' : '로그인',
@@ -27,10 +24,39 @@ export default class UserPage {
 		this.$section = new Section({
 			$parent: this.$parent,
 			initialState: this.isUserLogin() ? this.state : '',
+			onClick: (e, inputValue) => {
+				if (this.isUserLogin()) {
+					// logout api 호출 (쿠키 제거)만 하면 끝
+					navigateTo('/');
+				} else {
+					e.preventDefault();
+					if (inputValue.length === 0) {
+						this.$section.$alert.classList.add('active');
+						setTimeout(() => {
+							this.$section.$alert.classList.remove('active');
+						}, 2000);
+					} else {
+						console.log('login');
+						// login api 호출
+						// 성공시 : (쿠키 삽입)만 하면 끝 + navigateTo('/');
+						// 실패시 : 경고문구 || 길이가 0이거나
+					}
+				}
+			},
 		});
 
 		setTimeout(() => {
 			this.$parent.classList.add('active');
 		}, 0);
+	}
+
+	setState() {
+		this.$appBar.setState(this.state);
+		this.$section.setState(this.state);
+	}
+
+	isUserLogin() {
+		/* 유저가 로그인 되어 있다면, true 반환 아니면 false */
+		return this.state.length > 0;
 	}
 }
