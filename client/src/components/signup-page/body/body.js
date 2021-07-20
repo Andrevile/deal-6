acb89ad61895b87d104a5a501791ad84124353bb
jihoMoon <sun_days@naver.com>
2021-07-20 17:22:52 +0900

    [#16] feat: :sparkles: 인풋입력에 따른 버튼 색 구현

--- client/src/components/signup-page/body/body.js
+++ client/src/components/signup-page/body/body.js
@@ -18,16 +18,33 @@ export default class BodyPart {
         <div>
             아이디
         </div>
-        <input class='largeInput' type="text" placeholder="영문 숫자 조합 20자 이하">
+        <input class='largeInput' id='id' type="text" placeholder="영문 숫자 조합 20자 이하">
         <div>
             우리 동네
         </div>
-        <input class='largeInput' type="text" placeholder="시∙구 제외, 동만 입력">
+        <input class='largeInput' id='location' type="text" placeholder="시∙구 제외, 동만 입력">
         `;
+
+		this.$id = document.querySelector('#id');
+		this.$location = document.querySelector('#location');
 
 		this.button = new Button({
 			$parent: this.$target,
 			initialState: mode,
+			onClick: () => {},
+		});
+
+		this.$target.addEventListener('keyup', (e) => {
+			if (e.target.id === 'id' || e.target.id === 'location') {
+				if (
+					this.$id.value.length > 0 &&
+					this.$location.value.length > 0
+				) {
+					this.button.$target.classList.remove('disable');
+				} else {
+					this.button.$target.classList.add('disable');
+				}
+			}
 		});
 	}
 }
