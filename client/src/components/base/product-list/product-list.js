026c235a268b9bf09a689c611b447e0cae2e2a8d
jihoMoon <sun_days@naver.com>
2021-07-20 12:08:06 +0900

    [#12] feat: :sparkles: InterSectionObserver API
    - lazy loading
    - infinite scroll
    에 사용

--- client/src/components/base/product-list/product-list.js
+++ client/src/components/base/product-list/product-list.js
@@ -3,11 +3,12 @@ import { createDOMWithSelector } from '../../../util/createDOMWithSelector';
 export default class ProductLists {
 	state = [];
 
-	constructor({ $parent, initialState }) {
+	constructor({ $parent, initialState, refreshState }) {
 		this.state = initialState;
 		this.$target = createDOMWithSelector('section', '.productOutline');
 		$parent.appendChild(this.$target);
 
+		this.refreshState = refreshState;
 		this.render();
 	}
 
@@ -17,42 +18,47 @@ export default class ProductLists {
 	}
 
 	render() {
-		const result = this.state
-			.map((product) => {
-				return `
-				<article class='product'>
-					
-					<img class='product__imgs' src=${product.imgPath} data-link='/signup'>
-				
-					<div class='product__info'>
-						<span class='product__name' data-link='/detail'>${product.name}</span>
-						<div>
-							<span class='product__location' data-link='/detail'>${product.location} ∙</span>
-							<span class='product__time' data-link='/location'>${product.time}</span>
-						</div>
-						<span class='product__price' data-link='/location'>${product.price}</span>
-					</div>
-					
-					${this.createLikeButton(product.like)}
+		this.$target.innerHTML = this.createView();
+		this.observeTag();
+	}
 
-					<div class='rightBottom' >
-						${this.createChatCount(product.chatCount)}
-						${this.createLikeCount(product.likeCount)}
+	createView() {
+		return (
+			this.state
+				.map((product) => {
+					return `
+			<article class='product'>
+				
+				<img class='product__imgs' src=${product.imgPath} data-link='/signup'>
+			
+				<div class='product__info'>
+					<span class='product__name' data-link='/detail'>${product.name}</span>
+					<div>
+						<span class='product__location' data-link='/detail'>${product.location} ∙</span>
+						<span class='product__time' data-link='/location'>${product.time}</span>
 					</div>
-						
-				</article>
-			`;
-			})
-			.join('');
+					<span class='product__price' data-link='/location'>${product.price}</span>
+				</div>
+				
+				${this.createLikeButton(product.like)}
+
+				<div class='rightBottom' >
+					${this.createChatCount(product.chatCount)}
+					${this.createLikeCount(product.likeCount)}
+				</div>
+					
+			</article>
+		`;
+				})
+				.join('') + `<div id="end" class='product'></div>`
+		);
 
 		/*
-				고려할 부분!
-				1. user와 그 상품 주인인 경우 : like 대신 ':' 아이콘
-				2. product에 user id field 넣어서 userid와 비교한다.
-				3. like 모델이 따로 필요할듯? user와 product를 엮은
-			*/
-
-		this.$target.innerHTML = result;
+			고려할 부분!
+			1. user와 그 상품 주인인 경우 : like 대신 ':' 아이콘
+			2. product에 user id field 넣어서 userid와 비교한다.
+			3. like 모델이 따로 필요할듯? user와 product를 엮은
+		*/
 	}
 
 	createLikeButton(like) {
@@ -76,6 +82,46 @@ export default class ProductLists {
 			? `<img class='rightBottom__like' src='/icons/favorite_border_mini.svg' />
 						<span>${count}</span>`
 			: ``;
+	}
+
+	observeTag() {
+		// lazy loading , infinite Scroll 담당
+		const observerCallback = (entries, observer) => {
+			entries.forEach(async (entry) => {
+				if (entry.isIntersecting) {
+					console.log(entry.target.id);
+					if (entry.target.id === 'end') {
+						observer.unobserve(entry.target);
+
+						endTag.classList.add('spinner');
+						endTag.innerHTML = `<img class='rightBottom__like' src='/icons/loading-loader-svgrepo-com.svg' />`;
+
+						// const data = await api.randomCats();
+						// if (data.success) {
+
+						// if (data.data.length > 0) {
+						// 	this.refreshState([...this.state, ...data.data]);
+						//  endTag.innerHTML = ''
+						// endTag.classList.remove('spinner');
+						// } else {
+						// endTag.classList.remove('spinner');
+						// endTag.innerText = `❗No More Stuff❗`;
+
+						// }
+
+						// } else {
+						// 	alert(data.message);
+						// }
+					} else {
+						observer.unobserve(entry.target);
+					}
+				}
+			});
+		};
+		const endTag = this.$target.querySelector('#end');
+		const items = this.$target.querySelectorAll('.product');
+		const observer = new IntersectionObserver(observerCallback);
+		items.forEach((item) => observer.observe(item));
 	}
 
 	open() {
