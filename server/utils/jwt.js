fd42c0a19a7664c557b0438ded54de140d245bc7
0woodev <tc_yeongu.n@woowafriends.com>
2021-07-22 03:29:21 +0900

    [#39] doc: :books: createJWT에 jsDoc 부분 추가

--- server/utils/jwt.js
+++ server/utils/jwt.js
@@ -2,6 +2,13 @@ const jwt = require('jsonwebtoken');
 const config = require('../config/config');
 const { UserRepository } = require('../repository/user-repository');
 
+/**
+ *
+ * @param {object} user - DB 에서 조회한 결과
+ * @param {string} user.id - user의 id
+ * @param {string} user.pk - user의 pk
+ * @returns {string} JWT 입니다.
+ */
 const createJWT = (user) => {
 	const payload = {
 		pk: user.pk,
