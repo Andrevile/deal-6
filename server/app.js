4fe6537ce96eb0dd335bdaa5430368bae4008e80
0woodev <tc_yeongu.n@woowafriends.com>
2021-07-22 01:56:48 +0900

    [#18] set: :file_folder: bodyparser 미들웨어 추가 및 routes.index 로 묶기

--- server/app.js
+++ server/app.js
@@ -1,14 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const path = require('path');
-const app = express();
-// eslint-disable-next-line no-unused-vars
-const db = require('./db/db');
 
+const errorHandler = require('./middlewares/error-handler');
+const router = require('./routes');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+
 // app.use('/public', express.static(path.join(__dirname, 'src', 'public'))); // 경로 미정
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use('/api', router);
+
+app.use(errorHandler);
 
 app.use(express.static(path.resolve(__dirname, '../client', 'assets')));
 app.use(express.static(path.resolve(__dirname, '../client', 'dist')));
