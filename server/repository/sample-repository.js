91f4b388e1ec403392643ab2b28564139bee02bf
0woodev <tc_yeongu.n@woowafriends.com>
2021-07-22 04:58:25 +0900

    [#18] doc: :books:  샘플 레포지토리 QueryExecutor 반영

--- server/repository/sample-repository.js
+++ server/repository/sample-repository.js
@@ -1,24 +1,12 @@
-const { pool } = require('../db/db');
-const CustomError = require('../errors/custom-error');
-const error = require('../constants/error');
-const promiseHandler = require('../utils/promiseHandler');
+/* eslint-disable no-unused-vars */
+const { selectQueryExecutor } = require('../utils/queryExecutor');
 
 const sampleRepository = {
 	findAll: async () => {
-		/* pool 에서 가지고 있는 connection 을 가져옵니다. */
-		const connection = await pool.getConnection();
 		/* DB 에 요청할 query 문을 작성합니다. */
 		const query = `query 를 작성해서 넣으면 됩니다. 끝에 세미콜론 잊지마세요!`;
-
-		const [result, err] = await promiseHandler(connection.query(query));
-
-		if (err) {
-			/* DB connection 에서 error 가 생기면 발생하는 경우 */
-			throw new CustomError(error.SAMPLE_ERROR);
-		}
-
-		const [samples] = result;
-		connection.release();
+		/* selectQueryExecutor, insertQuery... 등 추후 추가할 예정입니다. */
+		const samples = await selectQueryExecutor(query);
 
 		return samples;
 	},
