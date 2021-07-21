d3b209bb856a00fa0b694840b159c21854324231
0woodev <tc_yeongu.n@woowafriends.com>
2021-07-22 01:23:39 +0900

    [#39] set: :fie_folder: auth 대신 decodeJWT 로 파일명 변경

--- server/routes/detail-router.js
+++ server/routes/detail-router.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const decodeJWT = require('../middlewares/auth');
+const decodeJWT = require('../middlewares/decode-jwt');
 const {
 	getProductByPk,
 	deleteProductByPk,
