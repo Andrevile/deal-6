164b29b813c38144f78daf356905953eff9cc4b6
0woodev <tc_yeongu.n@woowafriends.com>
2021-07-22 02:04:28 +0900

    [#18] refactor: :hammer: promise Handler 반영

--- server/repository/sample-repository.js
+++ server/repository/sample-repository.js
@@ -1,6 +1,7 @@
 const { pool } = require('../db/db');
 const CustomError = require('../errors/custom-error');
 const error = require('../constants/error');
+const promiseHandler = require('../utils/promiseHandler');
 
 const sampleRepository = {
 	findAll: async () => {
@@ -9,14 +10,7 @@ const sampleRepository = {
 		/* DB 에 요청할 query 문을 작성합니다. */
 		const query = `query 를 작성해서 넣으면 됩니다. 끝에 세미콜론 잊지마세요!`;
 
-		const [result, err] = await connection
-			.query(query)
-			.then((data) => {
-				return [data, null];
-			})
-			.catch((err) => {
-				return [null, err];
-			});
+		const [result, err] = await promiseHandler(connection.query(query));
 
 		if (err) {
 			/* DB connection 에서 error 가 생기면 발생하는 경우 */
