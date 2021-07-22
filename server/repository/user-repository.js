a1595e5cad3ddcbed0ee3a483fab3ab1236c257d
0woodev <tc_yeongu.n@woowafriends.com>
2021-07-22 22:22:40 +0900

    [#71] wip: :gear: user-repository create 함수제작 및 리팩토링

--- server/repository/user-repository.js
+++ server/repository/user-repository.js
@@ -1,18 +1,30 @@
-const { selectQueryExecutor } = require('../utils/queryExecutor');
+const {
+	selectQueryExecutor,
+	insertQueryExecutor,
+} = require('../utils/queryExecutor');
 
 const userRepository = {
 	findAll: async () => {
-		const query = `SELECT username FROM api_server.user;`;
+		const query = `SELECT user.id, user.pk, user.createdAt, user.updatedAt FROM api_server.user;`;
 
-		const users = await selectQueryExecutor(query);
-		return users;
+		return await selectQueryExecutor(query);
 	},
 
 	findOne: async (id) => {
-		const query = `SELECT username, id, pk, createdAt  FROM api_server.user where id='${id}';`;
+		const query = `SELECT user.id, user.pk, user.createdAt, user.updatedAt FROM api_server.user where id='${id}';`;
 
-		let [user] = await selectQueryExecutor(query);
-		return user;
+		return await selectQueryExecutor(query);
+	},
+
+	create: async (id, location) => {
+		const query = `
+		INSERT 
+			INTO api_server.user (id, MainLocation) 
+		VALUES
+			('${id}', '${location}');
+		`;
+
+		return await insertQueryExecutor(query);
 	},
 };
 
